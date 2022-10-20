d143ae3bd373c04674938e60ffe5e3c029548d6a
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-10-20 17:38:48 -0400

    Add some Gameboard tests to place ships on tiles

--- src/models/Gameboard.js
+++ src/models/Gameboard.js
@@ -1,3 +1,73 @@
-const Gameboard = () => {};
+const Ship = require('./Ship');
+
+const Gameboard = () => {
+  // Creates the grid
+  const grid = {};
+  for (let code = 65; code < 75; code++) {
+    grid[String.fromCharCode(code)] = [...new Array(10)].map(() => ({
+      ship: null,
+      missed: false,
+    }));
+  }
+
+  /**
+   * Places a new ship on the gameboard.
+   * @param {number} len
+   * @param {object} pos
+   * @returns true if successful, false otherwise.
+   */
+  const placeShip = (len, pos) => {
+    try {
+      const ship = Ship(len, pos);
+      const shipOrigin = ship.getPosition().origin;
+      const originLetter = shipOrigin.slice(0, 1);
+      const originNum = parseInt(shipOrigin.slice(1), 10);
+      const shipDirection = ship.getPosition().direction;
+
+      if (shipDirection === 'UP') {
+        let code = originLetter.charCodeAt(0);
+        for (let i = 0; i < len; i++) {
+          grid[String.fromCharCode(code)][originNum] = {
+            ship,
+            missed: false,
+          };
+          // ASCII code is decremented to move up rows
+          code--;
+        }
+      }
+
+      if (shipDirection === 'DOWN') {
+        let code = originLetter.charCodeAt(0);
+        for (let i = 0; i < len; i++) {
+          grid[String.fromCharCode(code)][originNum] = {
+            ship,
+            missed: false,
+          };
+          // ASCII code is incremented to move down rows
+          code++;
+        }
+      }
+    } catch (err) {
+      return false;
+    }
+
+    return true;
+  };
+
+  /**
+   * @param {string} tile A tile on the grid (e.g. 'A1').
+   * @returns Object containing tile properties.
+   */
+  const getTile = (tile) => {
+    const letter = tile.slice(0, 1);
+    const num = parseInt(tile.slice(1), 10);
+    return grid[letter][num];
+  };
+
+  return {
+    placeShip,
+    getTile,
+  };
+};
 
 module.exports = Gameboard;
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -1,3 +1,53 @@
 const Gameboard = require('../src/models/Gameboard');
 
-describe('Gameboard', () => {});
+describe('Gameboard', () => {
+  test('Can place new ship on gameboard (up)', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(5, { origin: 'E1', direction: 'UP' });
+    expect(gameboard.getTile('E1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'E1'
+    );
+    expect(gameboard.getTile('D1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'E1'
+    );
+    expect(gameboard.getTile('C1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'E1'
+    );
+    expect(gameboard.getTile('B1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'E1'
+    );
+    expect(gameboard.getTile('A1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'E1'
+    );
+  });
+
+  test('Can place new ship on gameboard (down)', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(5, { origin: 'A1', direction: 'DOWN' });
+    expect(gameboard.getTile('A1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'A1'
+    );
+    expect(gameboard.getTile('B1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'A1'
+    );
+    expect(gameboard.getTile('C1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'A1'
+    );
+    expect(gameboard.getTile('D1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'A1'
+    );
+    expect(gameboard.getTile('E1').ship.getPosition()).toHaveProperty(
+      'origin',
+      'A1'
+    );
+  });
+});
