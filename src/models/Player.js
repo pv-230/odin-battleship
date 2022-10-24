1a1047dbde5e9d3b327ff724da0a0402e4c4ba39
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-10-24 18:16:57 -0400

    Implement random moves for computer player

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,10 +1,36 @@
+/**
+ * Represents a player. If no name is given, a computer player will be set up.
+ * @param {string} n Player name. If empty, player becomes a computer.
+ * @returns The Player interface.
+ */
 const Player = (n) => {
   const name = n || 'Computer';
+
+  // Populates valid moves for the computer player
+  const moves = [];
+  if (!n) {
+    for (let char = 65; char <= 74; char++) {
+      for (let num = 1; num <= 10; num++) {
+        moves.push(String.fromCharCode(char).concat(num.toString()));
+      }
+    }
+  }
 
   const getName = () => name;
 
   const attack = (gameboard, tile = null) => {
+    // Player attacks a specific tile
     if (tile) return gameboard.receiveAttack(tile) ? tile : null;
+
+    // Computer player attacks random tile
+    if (moves.length === 0) throw new Error('No valid moves remaining');
+    const randomIndex = Math.floor(Math.random() * moves.length);
+    const randomTile = moves.at(randomIndex);
+    if (gameboard.receiveAttack(randomTile)) {
+      moves.splice(randomIndex, 1);
+      return randomTile;
+    }
+
     return null;
   };
 
--- test/Player.test.js
+++ test/Player.test.js
@@ -31,4 +31,13 @@ describe('Player', () => {
     const attackedTile = player.attack(gameboard);
     expect(gameboard.getTile(attackedTile).status).toBe(1);
   });
+
+  test('Player can make 100 random attacks', () => {
+    const gameboard = Gameboard();
+    const player = Player();
+    for (let i = 0; i < 100; i++) {
+      expect(() => player.attack(gameboard)).not.toThrow();
+    }
+    expect(() => player.attack(gameboard)).toThrow('No valid moves remaining');
+  });
 });
