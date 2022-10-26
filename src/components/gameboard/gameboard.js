9687bb644dc5471ecbb6429b9c24f2f62165a4d4
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-10-26 18:59:18 -0400

    Add grid displays for both players

--- src/components/gameboard/gameboard.js
+++ src/components/gameboard/gameboard.js
@@ -7,7 +7,19 @@ for (; charCode <= 74; charCode++) {
   for (let col = 1; col <= 10; col++) {
     const tile = document.createElement('div');
     tile.classList.add('gameboard__tile');
-    tile.textContent = `${String.fromCharCode(charCode)}${col}`;
+    tile.setAttribute('data-tile', `${String.fromCharCode(charCode)}${col}`);
     gameboardLeft.appendChild(tile);
+  }
+}
+
+// Generates the right gameboard
+const gameboardRight = document.querySelector('.gameboard_right');
+charCode = 65;
+for (; charCode <= 74; charCode++) {
+  for (let col = 1; col <= 10; col++) {
+    const tile = document.createElement('div');
+    tile.classList.add('gameboard__tile');
+    tile.setAttribute('data-tile', `${String.fromCharCode(charCode)}${col}`);
+    gameboardRight.appendChild(tile);
   }
 }
