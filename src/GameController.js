6e4cf7540e60d24169cf8cc5aab786c5d2cd0bd1
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-10-26 20:15:26 -0400

    Add gameboard markings

---
+++ src/GameController.js
@@ -0,0 +1,22 @@
+import Player from './models/Player';
+import Gameboard from './models/Gameboard';
+import { displayBoard } from './components/gameboard/gameboard';
+
+const player = Player('Bob');
+const computer = Player();
+const playerBoard = Gameboard();
+const computerBoard = Gameboard();
+
+playerBoard.placeShip(5, { origin: 'A1', direction: 'DOWN' });
+playerBoard.placeShip(4, { origin: 'A3', direction: 'DOWN' });
+playerBoard.placeShip(3, { origin: 'A5', direction: 'DOWN' });
+playerBoard.placeShip(3, { origin: 'A7', direction: 'DOWN' });
+playerBoard.placeShip(2, { origin: 'A9', direction: 'DOWN' });
+computerBoard.placeShip(5, { origin: 'J1', direction: 'UP' });
+computerBoard.placeShip(4, { origin: 'J3', direction: 'UP' });
+computerBoard.placeShip(3, { origin: 'J5', direction: 'UP' });
+computerBoard.placeShip(3, { origin: 'J7', direction: 'UP' });
+computerBoard.placeShip(2, { origin: 'J9', direction: 'UP' });
+
+displayBoard(playerBoard, true);
+displayBoard(computerBoard, false);
--- src/components/gameboard/gameboard.js
+++ src/components/gameboard/gameboard.js
@@ -1,25 +1,49 @@
 import './gameboard.css';
+import '../../models/Gameboard';
+import '../../models/Player';
 
-// Generates the left gameboard
-const gameboardLeft = document.querySelector('.gameboard_left');
-let charCode = 65;
-for (; charCode <= 74; charCode++) {
-  for (let col = 1; col <= 10; col++) {
-    const tile = document.createElement('div');
-    tile.classList.add('gameboard__tile');
-    tile.setAttribute('data-tile', `${String.fromCharCode(charCode)}${col}`);
-    gameboardLeft.appendChild(tile);
-  }
-}
+/**
+ *
+ * @param {object} gb Gameboard object.
+ * @param {object} p Player object.
+ */
+const displayBoard = (gameboard, showShips) => {
+  let gameboardDOM;
 
-// Generates the right gameboard
-const gameboardRight = document.querySelector('.gameboard_right');
-charCode = 65;
-for (; charCode <= 74; charCode++) {
-  for (let col = 1; col <= 10; col++) {
-    const tile = document.createElement('div');
-    tile.classList.add('gameboard__tile');
-    tile.setAttribute('data-tile', `${String.fromCharCode(charCode)}${col}`);
-    gameboardRight.appendChild(tile);
+  // Selects the appropriate board
+  if (showShips) {
+    gameboardDOM = document.querySelector('.gameboard_left');
+  } else {
+    gameboardDOM = document.querySelector('.gameboard_right');
   }
-}
+
+  let charCode = 65;
+  for (; charCode <= 74; charCode++) {
+    for (let col = 1; col <= 10; col++) {
+      const tileStr = `${String.fromCharCode(charCode)}${col}`;
+      const tile = document.createElement('div');
+
+      tile.classList.add('gameboard__tile');
+      tile.setAttribute('data-tile', tileStr);
+
+      // Marks ship postions for player's board
+      if (showShips && gameboard.getTile(tileStr).ship) {
+        tile.textContent = 'S';
+      }
+
+      // Marks misses
+      if (gameboard.getTile(tileStr).status === 1) {
+        tile.textContent = 'M';
+      }
+
+      // Marks hits
+      if (gameboard.getTile(tileStr).status === 2) {
+        tile.textContent = 'H';
+      }
+
+      gameboardDOM.appendChild(tile);
+    }
+  }
+};
+
+export { displayBoard };
