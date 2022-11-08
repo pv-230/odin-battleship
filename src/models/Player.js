ca4be1eabffc088a6eee4053515de47f70d2200b
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-07 20:09:23 -0500

    Improve computer's decision making

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,4 +1,5 @@
 const { toGridCoord, toTileStr } = require('../utility/stringConversion');
+const { PlayerErrors } = require('../utility/errors');
 
 /**
  * Represents a player. If no name is given, a computer player will be set up.
@@ -20,6 +21,40 @@ const Player = (n) => {
   const nextAttacks = [];
 
   /**
+   * Adds the next attacks that the computer should make if the tileStr represents a hit.
+   * @param {string} tileStr String that represents a tile (e.g. 'A1').
+   * @param {Object} gameboard Gameboard object.
+   */
+  const setNextAttacks = (tileStr, gameboard) => {
+    const { row, col } = toGridCoord(tileStr);
+    nextAttacks.splice(0);
+
+    // Checks up
+    let tile = gameboard.getTile(toTileStr(row - 1, col));
+    if (tile && tile.status === 0) {
+      nextAttacks.push(toTileStr(row - 1, col));
+    }
+
+    // Checks to the right
+    tile = gameboard.getTile(toTileStr(row, col + 1));
+    if (tile && tile.status === 0) {
+      nextAttacks.push(toTileStr(row, col + 1));
+    }
+
+    // Checks down
+    tile = gameboard.getTile(toTileStr(row + 1, col));
+    if (tile && tile.status === 0) {
+      nextAttacks.push(toTileStr(row + 1, col));
+    }
+
+    // Checks to the left
+    tile = gameboard.getTile(toTileStr(row, col - 1));
+    if (tile && tile.status === 0) {
+      nextAttacks.push(toTileStr(row, col - 1));
+    }
+  };
+
+  /**
    * @returns The player's name.
    */
   const getName = () => name;
@@ -33,58 +68,55 @@ const Player = (n) => {
    * Allows a player to attack a gameboard at a specific tile or at a random
    * tile.
    * @param {object} gameboard The gameboard to attack.
-   * @param {string} tile The tile to attack (e.g. 'A1').
-   * @returns The tile that was successfully attack or null otherwise.
+   * @param {string} [tileStr] The tile to attack (e.g. 'A1'). If omitted, a random attack is made.
+   * @returns The tile that was attacked.
    */
-  const attack = (gameboard, tile = null) => {
+  const attack = (gameboard, tileStr = null) => {
     // Player attacks a specific tile
-    if (tile) return gameboard.receiveAttack(tile) ? tile : null;
+    if (tileStr) return gameboard.receiveAttack(tileStr) ? tileStr : null;
+
+    // Checks if computer can make additional moves
+    if (moves.length === 0) throw new Error(PlayerErrors.noValidMoves);
+
+    // Computer player chooses an attack from the nextAttacks array if not empty
+    if (nextAttacks.length > 0) {
+      const randomIndex = Math.floor(Math.random() * nextAttacks.length);
+      const nextTileStr = nextAttacks[randomIndex];
+      if (!gameboard.receiveAttack(nextTileStr)) throw new Error(PlayerErrors.compAttackFailed);
+
+      // Removes the attacked tile from future options
+      moves.splice(moves.indexOf(nextTileStr), 1);
+      nextAttacks.splice(randomIndex, 1);
+
+      const attackedTile = gameboard.getTile(nextTileStr);
+      if (attackedTile.status === 2 && attackedTile.ship && !attackedTile.ship.isSunk()) {
+        setNextAttacks(nextTileStr, gameboard);
+        console.log(`Targeted hit on ${nextTileStr}`);
+        console.log(`Next attacks is now: ${nextAttacks}`);
+      } else if (attackedTile.ship && attackedTile.ship.isSunk()) {
+        nextAttacks.splice(0);
+        console.log('Ship sunk');
+        console.log(`Next attacks is now: ${nextAttacks}`);
+      }
+
+      return nextTileStr;
+    }
 
     // Computer player attacks random tile
-    if (moves.length === 0) throw new Error('No valid moves remaining');
     const randomIndex = Math.floor(Math.random() * moves.length);
     const randomTile = moves.at(randomIndex);
-    if (gameboard.receiveAttack(randomTile)) {
-      if (gameboard.getTile(randomTile).status === 2) {
-        setNextAttacks(randomTile, gameboard);
-      }
-      moves.splice(randomIndex, 1);
-      return randomTile;
+    if (!gameboard.receiveAttack(randomTile)) throw new Error(PlayerErrors.compAttackFailed);
+
+    // Checks if computer hit a ship
+    if (gameboard.getTile(randomTile).status === 2) {
+      // Allows computer to choose tiles near damaged ship
+      setNextAttacks(randomTile, gameboard);
+      console.log(`Random hit on ${randomTile}`);
+      console.log(`Next attacks: ${nextAttacks}`);
     }
 
-    return null;
-  };
-
-  /**
-   * Adds the next attacks that the computer should make if the tileStr represents a hit.
-   * @param {string} tileStr String that represents a tile (e.g. 'A1').
-   * @param {Object} gameboard Gameboard object.
-   */
-  const setNextAttacks = (tileStr, gameboard) => {
-    const { row, col } = toGridCoord(tileStr);
-    nextAttacks.splice(0);
-
-    // Checks up
-    if (gameboard.getTile(toTileStr(row - 1, col))) {
-      nextAttacks.push(toTileStr(row - 1, col));
-    }
-
-    // Checks to the right
-    if (gameboard.getTile(toTileStr(row, col + 1))) {
-      nextAttacks.push(toTileStr(row, col + 1));
-    }
-
-    // Checks down
-    if (gameboard.getTile(toTileStr(row + 1, col))) {
-      nextAttacks.push(toTileStr(row + 1, col));
-    }
-
-    // Checks to the left
-    if (gameboard.getTile(toTileStr(row, col - 1))) {
-      nextAttacks.push(toTileStr(row, col - 1));
-    }
-    console.log(`Hit on ${tileStr}`);
-    console.log(nextAttacks);
+    moves.splice(randomIndex, 1);
+    return randomTile;
   };
 
   return {
