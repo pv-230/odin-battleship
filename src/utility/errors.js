33893dc96da56e6ef32650e6462a986a5ac21807
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-11-07 20:07:53 -0500

    Move error file

---
+++ src/utility/errors.js
@@ -0,0 +1,18 @@
+const GameboardErrors = {
+  proximity: 'Ship cannot be placed too close to others',
+  invalidTileStr: 'Invalid tile string',
+  missingLengthArg: 'Missing length argument',
+  maxShipCount: 'Max ship count reached',
+};
+
+const ShipErrors = {
+  invalidShipLength: 'Invalid ship length',
+  invalidShipPosition: 'Invalid ship position',
+};
+
+const PlayerErrors = {
+  noValidMoves: 'No valid moves remaining',
+  compAttackFailed: 'Computer attack failed',
+};
+
+module.exports = { GameboardErrors, ShipErrors, PlayerErrors };
